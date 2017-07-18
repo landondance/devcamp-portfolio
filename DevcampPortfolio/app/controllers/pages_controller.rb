e18bedb55f108b591fcc6d4cbd7100dd18863e72
Jordan Hudgens <jordan@devcamp.com>
2017-07-18 16:41:13 +0000

    Implemented validations for blogs, skills, and portfolio items

--- DevcampPortfolio/app/controllers/pages_controller.rb
+++ DevcampPortfolio/app/controllers/pages_controller.rb
@@ -11,5 +11,5 @@ class PagesController < ApplicationController
   end
 
 end
-=======
+
 
--- DevcampPortfolio/app/models/skill.rb
+++ DevcampPortfolio/app/models/skill.rb
@@ -1,5 +1,4 @@
 class Skill < ApplicationRecord
-
+  validates_presence_of :title, :percent_utilized
 end
-=======
 
